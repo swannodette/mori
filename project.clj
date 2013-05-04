64de64ee8140851d1236a116a4cedaea32dd1a1f
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2013-05-04 03:45:43 -0500

    bump to 0.2.0-SNAPSHOT in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mori "0.1.0-SNAPSHOT"
+(defproject mori "0.2.0-SNAPSHOT"
   :description "Persistent Data Structures for JavaScript"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1798"]]
