5cb88e41141da311a97cc34a0727b8153a3ae6a5
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2013-05-04 03:42:00 -0500

    work on package.json and related enchancements for npm packaging, etc.

---
+++ spec/mori-spec.js
@@ -0,0 +1,8 @@
+var mori = require("../");
+
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        // will fail
+	expect(true).toBe(false);
+    });
+});
