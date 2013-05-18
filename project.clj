3e35a7201a281d74e9ac1e97b374652bde0718fd
David Nolen <david.nolen@gmail.com>
2013-05-18 12:15:20 -0400

    0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mori "0.2.0-SNAPSHOT"
+(defproject mori "0.2.0"
   :description "Persistent Data Structures for JavaScript"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1806"]]
