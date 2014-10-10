601fcf6b2be97ee3a6d414441a9eaaa7ef974ae9
dnolen <dnolen@cognitect.com>
2014-10-10 16:33:14 -0400

    bump cljs dev

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Persistent Data Structures for JavaScript"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2307"]]
+                 [org.clojure/clojurescript "0.0-2371"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
