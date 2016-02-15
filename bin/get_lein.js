b49ba77adeec0898f2a47cca28f5a599eaf3122f
Jesse Hallett <(none)>
2016-02-14 17:48:43 -0800

    Fix get_lein script
    The existing version of the script was giving me an empty file.

--- bin/get_lein.js
+++ bin/get_lein.js
@@ -1,19 +1,12 @@
 #!/usr/bin/env node
 
-var https = require("https"),
-    fs    = require("fs");
+var wget = require('wgetjs');
 
-var lein_loc = __dirname + "/lein";
-
-https.get("https://raw.github.com/technomancy/leiningen/stable/bin/lein", function(res) {
-    var body = "";
-    res.setEncoding("utf8");
-    res.on("data", function (chunk) {
-        body += chunk;
-    });
-    res.on("end", function () {
-        fs.writeFileSync(lein_loc, body);
-    });
-}).on("error", function(e) {
-    console.log("HTTP error: " + e.message);
+wget({
+    url: "https://raw.github.com/technomancy/leiningen/stable/bin/lein",
+    dest: __dirname + "/lein",
+}, function(err) {
+    if (err) {
+        console.log(err.message);
+    }
 });
